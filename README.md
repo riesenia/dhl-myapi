f9be050ef1bae2be8763763e4d3deb4d244b2cfd
segy <segy@riesenia.com>
2017-02-28 23:32:57 +0100

    init - wsdl modified to dhl

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# dhl-myapi
-DHL Parcel Slovakia MyApi PHP client implementation
+# DHL MyApi PHP Client
+
+DHL Parcel Slovakia MyApi PHP client implementation based on [salamek/ppl-my-api](https://github.com/Salamek/PplMyApi) package.
+
+## Installation
+
+Install the latest version using `composer require riesenia/dhl-myapi`
+
+## Usage
+
+See [salamek/ppl-my-api](https://github.com/Salamek/PplMyApi) README.
---
+++ src/Api.php
@@ -0,0 +1,49 @@
+<?php
+namespace Riesenia\DhlMyApi;
+
+use Salamek\PplMyApi\Api as PplApi;
+use Salamek\PplMyApi\Exception\OfflineException;
+use Salamek\PplMyApi\Exception\SecurityException;
+
+/**
+ * Class Client
+ *
+ * @author Tomas Saghy <segy@riesenia.com>
+ */
+class Api extends PplApi
+{
+    /** @var string */
+    private $wsdl = 'https://myapi.dhlparcel.sk/MyAPI.svc?wsdl';
+
+    /**
+     * MyApi constructor
+     *
+     * @param null|string $username
+     * @param null|string $password
+     * @param null|integer $customerId
+     */
+    public function __construct($username = null, $password = null, $customerId = null)
+    {
+        if (strlen($username) > 32) {
+            throw new SecurityException('Username is longer then 32 characters');
+        }
+        if (strlen($password) > 32) {
+            throw new SecurityException('Password is longer then 32 characters');
+        }
+
+        $this->username = $username;
+        $this->password = $password;
+        $this->customerId = $customerId;
+        $this->securedStorage = sys_get_temp_dir() . '/' . __CLASS__;
+
+        try {
+            $this->soap = new \SoapClient($this->wsdl);
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to build soap client');
+        }
+
+        if (!$this->isHealthy()) {
+            throw new OfflineException('DHL MyAPI is offline');
+        }
+    }
+}
